1743ce4153c50847f3b26774136dd6b9a0a1c0bf
Jeff Nettleton <jeffdn@gmail.com>
2016-07-21 00:33:52 -0400

    implement everything as a class

--- first/src/main.rs
+++ first/src/main.rs
@@ -24,141 +24,196 @@ pub enum Color {
     BoldBlue,
 }
 
-pub fn color_text(text: &str, color: Color) -> String {
-    let clrstr = match color {
-        Color::White       => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[37m", text)        },
-        Color::BoldWhite   => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[1m\x1B[33m\x1B[37m", text) },
-        Color::BoldRed     => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[1m\x1B[33m\x1B[31m", text) },
-        Color::BoldGreen   => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[1m\x1B[33m\x1B[32m", text) },
-        Color::BoldBlue    => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[1m\x1B[33m\x1B[34m", text) },
-    };
-
-    clrstr
+struct TableDesc {
+    names:  Vec<String>,
+    types:  Vec<types::Type>,
+    widths: Vec<usize>,
+    data:   Vec<Vec<String>>,
 }
 
-fn parse_result(column: &postgres::rows::Row, colpos: usize, coltype: &types::Type) -> String {
-    let val: String = match *coltype {
-        types::Type::Text | types::Type::Varchar => column.get(colpos),
-        types::Type::Bool => {
-            let tmpbool: bool = column.get(colpos);
-            match tmpbool {
-                true  => String::from("true"),
-                false => String::from("false"),
-            }
-        },
-        types::Type::Int2 | types::Type::Int4 => {
-            let tmpint: i32 = column.get(colpos);
-            tmpint.to_string()
-        },
-        types::Type::Int8 => {
-            let tmpint: i64 = column.get(colpos);
-            tmpint.to_string()
-        },
-        types::Type::Float4 => {
-            let tmpflt: f32 = column.get(colpos);
-            tmpflt.to_string()
-        },
-        types::Type::Float8 => {
-            let tmpflt: f64 = column.get(colpos);
-            tmpflt.to_string()
-        },
-        types::Type::Timestamp => {
-            let tmpdate: chrono::NaiveDateTime = column.get(colpos);
-            tmpdate.format("%Y-%m-%d %H:%M:%s").to_string()
-        },
-        types::Type::TimestampTZ => {
-            let tmpdate: chrono::DateTime<chrono::UTC> = column.get(colpos);
-            tmpdate.format("%Y-%m-%d %H:%M:%s").to_string()
-        },
-        types::Type::Date => {
-            let tmpdate: chrono::NaiveDate = column.get(colpos);
-            tmpdate.format("%Y-%m-%d").to_string()
-        },
-        _ => String::from(""),
-    };
 
-    val
-}
-
-pub fn get_alignment(coltype: &types::Type) -> Align {
-    match coltype {
-        &types::Type::Int2          => Align::Right,
-        &types::Type::Int4          => Align::Right,
-        &types::Type::Int8          => Align::Right,
-        &types::Type::Float4        => Align::Right,
-        &types::Type::Float8        => Align::Right,
-        &types::Type::Date          => Align::Right,
-        &types::Type::Timestamp     => Align::Right,
-        &types::Type::TimestampTZ   => Align::Right,
-        _                           => Align::Left,
-    }
-}
-
-pub fn pad_gen(len: usize, pad: &str) -> String {
-    let mut padstr = String::from("");
-
-    for _ in 0..len {
-        padstr = padstr + pad;
+impl TableDesc {
+    fn new() -> TableDesc {
+        TableDesc {
+            names:  Vec::new(),
+            types:  Vec::new(),
+            widths: Vec::new(),
+            data:   Vec::new(),
+        }
     }
 
-    padstr
-}
+    fn parse_result(&self, column: &postgres::rows::Row, colpos: usize) -> String {
+        let val: String = match self.types[colpos] {
+            types::Type::Text | types::Type::Varchar => column.get(colpos),
+            types::Type::Bool => {
+                let tmpbool: bool = column.get(colpos);
+                match tmpbool {
+                    true  => String::from("true"),
+                    false => String::from("false"),
+                }
+            },
+            types::Type::Int2 | types::Type::Int4 => {
+                let tmpint: i32 = column.get(colpos);
+                tmpint.to_string()
+            },
+            types::Type::Int8 => {
+                let tmpint: i64 = column.get(colpos);
+                tmpint.to_string()
+            },
+            types::Type::Float4 => {
+                let tmpflt: f32 = column.get(colpos);
+                tmpflt.to_string()
+            },
+            types::Type::Float8 => {
+                let tmpflt: f64 = column.get(colpos);
+                tmpflt.to_string()
+            },
+            types::Type::Timestamp => {
+                let tmpdate: chrono::NaiveDateTime = column.get(colpos);
+                tmpdate.format("%Y-%m-%d %H:%M:%s").to_string()
+            },
+            types::Type::TimestampTZ => {
+                let tmpdate: chrono::DateTime<chrono::UTC> = column.get(colpos);
+                tmpdate.format("%Y-%m-%d %H:%M:%s").to_string()
+            },
+            types::Type::Date => {
+                let tmpdate: chrono::NaiveDate = column.get(colpos);
+                tmpdate.format("%Y-%m-%d").to_string()
+            },
+            _ => String::from(""),
+        };
 
-pub fn format_field(column: &str, width: usize, align: Align) -> String {
-    let padlen: usize = width - column.len();
-    let extra: usize = padlen % 2;
+        val
+    }
 
-    let ret: String = match align {
-        Align::Center   => { format!("{}{}{}", pad_gen(padlen/2, " "), column, pad_gen((padlen/2)+extra, " ")) },
-        Align::Right    => { format!("{}{}", pad_gen(padlen, " "), column) },
-        Align::Left     => { format!("{}{}", column, pad_gen(padlen, " ")) },
-    };
+    fn append(&mut self, row: &postgres::rows::Row) {
+        let mut colvals: Vec<String> = Vec::new();
 
-    ret
-}
+        for i in 0..row.len() {
+            let ret = row.get_bytes(i);
+            let col = match ret.is_some() {
+                true    => self.parse_result(&row, i),
+                false   => String::from(""),
+            };
 
-fn print_row(coltypes: &Vec<types::Type>, colwidths: &Vec<usize>, rowdata: &Vec<String>, is_header: bool) {
-    for (i, col) in rowdata.iter().enumerate() {
-        if i > 0 {
-            print!("{}", color_text("|", Color::BoldWhite));
+            self.widths[i] = match col.len() > self.widths[i] {
+                true    => col.len(),
+                false   => self.widths[i]
+            };
+
+            colvals.push(col);
         }
 
-        match is_header {
-            false   => print!(" {} ", format_field(&col, colwidths[i], get_alignment(&coltypes[i]))),
-            true    => print!(" {} ", color_text(&format_field(&col, colwidths[i], Align::Center), Color::BoldWhite)),
-        };
+        self.data.push(colvals);
     }
 
-    println!("");
-
-    match is_header {
-        false   => {},
-        true    => {
-            for (i, col) in colwidths.iter().enumerate() {
-                if i > 0 {
-                    print!("{}", color_text("+", Color::BoldWhite));
-                }
-
-                print!("{}", color_text(&pad_gen(col+2, "-"), Color::BoldWhite));
+    fn print_row(&self, rowdata: &Vec<String>, is_header: bool) {
+        for (i, col) in rowdata.iter().enumerate() {
+            if i > 0 {
+                print!("{}", TableDesc::color_text("|", Color::BoldWhite));
             }
 
-            println!("");
-        },
-    };
+            match is_header {
+                false   => print!(" {} ", TableDesc::format_field(&col,
+                                                            self.widths[i],
+                                                            TableDesc::get_alignment(&self.types[i]))),
+                true    => print!(" {} ", TableDesc::color_text(&TableDesc::format_field(&col,
+                                                                             self.widths[i],
+                                                                             Align::Center),
+                                                          Color::BoldWhite)),
+            };
+        }
 
+        println!("");
+
+        match is_header {
+            false   => {},
+            true    => {
+                for (i, col) in self.widths.iter().enumerate() {
+                    if i > 0 {
+                        print!("{}", TableDesc::color_text("+", Color::BoldWhite));
+                    }
+
+                    print!("{}", TableDesc::color_text(&TableDesc::pad_gen(col+2, "-"), Color::BoldWhite));
+                }
+
+                println!("");
+            },
+        };
+
+    }
+
+    fn print(&self) {
+        self.print_row(&self.names, true);
+        for rowdata in &self.data {
+            self.print_row(&rowdata, false);
+        }
+    }
 }
+
+/* public functions, for string formatting, etc. */
+impl TableDesc {
+    pub fn get_alignment(coltype: &types::Type) -> Align {
+        match coltype {
+            &types::Type::Int2          => Align::Right,
+            &types::Type::Int4          => Align::Right,
+            &types::Type::Int8          => Align::Right,
+            &types::Type::Float4        => Align::Right,
+            &types::Type::Float8        => Align::Right,
+            &types::Type::Date          => Align::Right,
+            &types::Type::Timestamp     => Align::Right,
+            &types::Type::TimestampTZ   => Align::Right,
+            _                           => Align::Left,
+        }
+    }
+
+    pub fn pad_gen(len: usize, pad: &str) -> String {
+        let mut padstr = String::from("");
+
+        for _ in 0..len {
+            padstr = padstr + pad;
+        }
+
+        padstr
+    }
+
+    pub fn format_field(column: &str, width: usize, align: Align) -> String {
+        let padlen: usize = width - column.len();
+        let extra: usize = padlen % 2;
+
+        let ret: String = match align {
+            Align::Right    => { format!("{}{}", TableDesc::pad_gen(padlen, " "), column) },
+            Align::Left     => { format!("{}{}", column, TableDesc::pad_gen(padlen, " ")) },
+            Align::Center   => { format!("{}{}{}", TableDesc::pad_gen(padlen/2, " "), column,
+                                                   TableDesc::pad_gen((padlen/2)+extra, " ")) },
+        };
+
+        ret
+    }
+
+    pub fn color_text(text: &str, color: Color) -> String {
+        let clrstr = match color {
+            Color::White       => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[37m", text)        },
+            Color::BoldWhite   => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[1m\x1B[33m\x1B[37m", text) },
+            Color::BoldRed     => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[1m\x1B[33m\x1B[31m", text) },
+            Color::BoldGreen   => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[1m\x1B[33m\x1B[32m", text) },
+            Color::BoldBlue    => { format!("{}{}\x1B[33m\x1B[0m", "\x1B[33m\x1B[1m\x1B[33m\x1B[34m", text) },
+        };
+
+        clrstr
+    }
+}
+
 
 fn main() {
     let mut buf = String::new();
-    let mut colnames: Vec<String> = Vec::new();
-    let mut coldata: Vec<Vec<String>> = Vec::new();
-    let mut colwidths: Vec<usize> = Vec::new();
-    let mut coltypes: Vec<types::Type> = Vec::new();
+    let mut tdesc = TableDesc::new();
     let mut args = std::env::args();
 
     if args.len() == 1 {
-        println!("{} {}", color_text("error:", Color::BoldRed), color_text("a DBURI is required!", Color::BoldWhite));
-        println!("{} cat file.sql | first [DBURI]", color_text("usage:", Color::BoldBlue));
+        println!("{} {}", TableDesc::color_text("error:", Color::BoldRed),
+                          TableDesc::color_text("a DBURI is required!", Color::BoldWhite));
+        println!("{} cat file.sql | first [DBURI]", TableDesc::color_text("usage:", Color::BoldBlue));
         std::process::exit(1);
     }
 
@@ -167,38 +222,17 @@ fn main() {
 
     let conn = Connection::connect(&connstr as &str, SslMode::None).unwrap();
     let res = &conn.query(&mut buf, &[]).unwrap();
-    let numcols = res.columns().len();
 
     for col in res.columns() {
         let colname = String::from(col.name());
-        colwidths.push(colname.len());
-        colnames.push(colname);
-        coltypes.push(col.type_().clone());
+        tdesc.widths.push(colname.len());
+        tdesc.names.push(colname);
+        tdesc.types.push(col.type_().clone());
     }
 
     for row in res.iter() {
-        let mut colvals: Vec<String> = Vec::new();
-
-        for i in 0..numcols {
-            let ret = row.get_bytes(i);
-            let col = match ret.is_some() {
-                true    => parse_result(&row, i, &coltypes[i]),
-                false   => String::from(""),
-            };
-
-            colwidths[i] = match col.len() > colwidths[i] {
-                true    => col.len(),
-                false   => colwidths[i]
-            };
-
-            colvals.push(col);
-        }
-
-        coldata.push(colvals);
+        tdesc.append(&row);
     }
 
-    print_row(&Vec::new(), &colwidths, &colnames, true);
-    for rowdata in coldata {
-        print_row(&coltypes, &colwidths, &rowdata, false);
-    }
+    tdesc.print();
 }
--- first/src/tests.rs
+++ first/src/tests.rs
@@ -1,27 +1,28 @@
 extern crate postgres;
 
+use TableDesc;
 use super::*;
 use postgres::types;
 
 #[test]
 fn test_color() {
-    assert_eq!("\x1B[33m\x1B[1m\x1B[33m\x1B[37mfoo\x1B[33m\x1B[0m", color_text("foo", Color::BoldWhite));
+    assert_eq!("\x1B[33m\x1B[1m\x1B[33m\x1B[37mfoo\x1B[33m\x1B[0m", TableDesc::color_text("foo", Color::BoldWhite));
 }
 
 #[test]
 fn test_alignment() {
-    assert_eq!(get_alignment(&types::Type::Int4), Align::Right);
+    assert_eq!(TableDesc::get_alignment(&types::Type::Int4), Align::Right);
 }
 
 #[test]
 fn test_padding() {
-    assert_eq!("     ", pad_gen(5, " "));
-    assert_eq!("-----", pad_gen(5, "-"));
+    assert_eq!("     ", TableDesc::pad_gen(5, " "));
+    assert_eq!("-----", TableDesc::pad_gen(5, "-"));
 }
 
 #[test]
 fn test_formatting() {
-    assert_eq!("foo   ", format_field("foo", 6, Align::Left));
-    assert_eq!("   foo", format_field("foo", 6, Align::Right));
-    assert_eq!(" foo  ", format_field("foo", 6, Align::Center));
+    assert_eq!("foo   ", TableDesc::format_field("foo", 6, Align::Left));
+    assert_eq!("   foo", TableDesc::format_field("foo", 6, Align::Right));
+    assert_eq!(" foo  ", TableDesc::format_field("foo", 6, Align::Center));
 }
