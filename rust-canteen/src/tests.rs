dde12ed6b172d98875ba05f477d498460d632df6
Jeff Nettleton <jeffdn@gmail.com>
2016-07-23 15:50:31 -0400

    some initial tests for the router

---
+++ rust-canteen/src/tests.rs
@@ -0,0 +1,38 @@
+extern crate regex;
+
+use Route;
+use regex::Regex;
+
+#[test]
+fn test_route_match_fail() {
+    let rt = Route::new("/api/v1/foo/<int:foo_id>");
+    assert_eq!(false, rt.is_match("/api/v1/bar"));
+    assert_eq!(false, rt.is_match("/api/v1/foo/asdf"));
+}
+
+#[test]
+fn test_route_match_simple() {
+    let rt = Route::new("/api/v1/foo/<foo_stuff>");
+    assert_eq!("blahblahblah", rt.parse("/api/v1/foo/blahblahblah").unwrap().get("foo_stuff").unwrap());
+}
+
+#[test]
+fn test_route_match_single_int() {
+    let rt = Route::new("/api/v1/foo/<int:foo_id>");
+    assert_eq!("123", rt.parse("/api/v1/foo/123").unwrap().get("foo_id").unwrap());
+}
+
+#[test]
+fn test_route_match_single_str() {
+    let rt = Route::new("/api/v1/foo/<str:foo_stuff>");
+    assert_eq!("blahblahblah", rt.parse("/api/v1/foo/blahblahblah").unwrap().get("foo_stuff").unwrap());
+}
+
+#[test]
+fn test_route_match_many() {
+    let rt = Route::new("/api/v1/foo/<int:foo_id>/bar/<str:bar>/baz/<int:baz_id>");
+    let rm = rt.parse("/api/v1/foo/123/bar/bar/baz/456").unwrap();
+    assert_eq!("123", rm.get("foo_id").unwrap());
+    assert_eq!("bar", rm.get("bar").unwrap());
+    assert_eq!("456", rm.get("baz_id").unwrap());
+}
