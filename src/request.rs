a81f774b1c78aa9aeecc322a495ddf86a0271abc
Jeff Nettleton <jeffdn@gmail.com>
2016-08-27 23:52:13 -0400

    add MIT license

--- src/request.rs
+++ src/request.rs
@@ -1,3 +1,11 @@
+/* Copyright (c) 2016
+ * Jeff Nettleton
+ *
+ * Licensed under the MIT license (http://opensource.org/licenses/MIT). This
+ * file may not be copied, modified, or distributed except according to those
+ * terms
+ */
+
 use std::collections::HashMap;
 
 #[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
--- src/route.rs
+++ src/route.rs
@@ -1,3 +1,11 @@
+/* Copyright (c) 2016
+ * Jeff Nettleton
+ *
+ * Licensed under the MIT license (http://opensource.org/licenses/MIT). This
+ * file may not be copied, modified, or distributed except according to those
+ * terms
+ */
+
 extern crate regex;
 
 use std::env;
