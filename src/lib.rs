2b8d11eaebe97fbc4940a08f602a272f443dd550
Jeff Nettleton <jeffdn@gmail.com>
2016-08-29 21:26:07 -0400

    add Rust-style documentation, update usage of internal APIs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,90 @@
-/* Copyright (c) 2016
- * Jeff Nettleton
- *
- * Licensed under the MIT license (http://opensource.org/licenses/MIT). This
- * file may not be copied, modified, or distributed except according to those
- * terms
- */
+// Copyright (c) 2016
+// Jeff Nettleton
+//
+// Licensed under the MIT license (http://opensource.org/licenses/MIT). This
+// file may not be copied, modified, or distributed except according to those
+// terms
+
+//! # Canteen
+//!
+//! ## Description
+//!
+//! Canteen is a simple clone of [Flask](http://flask.pocoo.org), a simple but powerful Python
+//! web framework. There is code for an example implementation in the repository located
+//! here: [canteen-impl](https://gitlab.com/jeffdn/canteen-impl).
+//!
+//! ## Usage
+//!
+//! It's by no means complete, but I'm working on it, and it's now available on
+//! [crates.io](https://crates.io/)! To install and check it out, add the following to
+//! your Cargo.toml:
+//!
+//! ```toml
+//! [dependencies]
+//! canteen = "0.2"
+//! ```
+//!
+//! ## Example
+//!
+//! The principle behind Canteen is simple -- handler functions are defined as simple
+//! Rust functions that take a `Request` and return a `Response`. Handlers are then attached
+//! to one or more routes and HTTP methods/verbs. Routes are specified using a simple
+//! syntax that lets you define variables within them; variables that can then be
+//! extracted to perform various operations. Currently, the following variable types can
+//! be used:
+//!
+//! - `<path:name>` will greedily take all path data contained
+//!   - ex: `cnt.add_route("/static/<path:name>", &[Method::Get], utils::static_file)` will
+//!   serve anything in the `/static/` directory as a file
+//! - `<int:name>` will return an integer from a path segment
+//!   - ex: `cnt.add_route("/api/foo/<int:foo_id>", &[Method::Get], my_handler)` will match
+//!   `"/api/foo/123"` but not `"/api/foo/123.34"` or `"/api/foo/bar"`
+//! - `<float:name>` does the same thing as the `int` parameter definition, but matches numbers
+//! with decimal points
+//! - `<str:name>` will match anything inside a path segment, except a forward slash
+//!
+//! ```rust,no_run
+//! extern crate canteen;
+//!
+//! use canteen::*;
+//! use canteen::utils;
+//!
+//! fn hello_handler(req: &Request) -> Response {
+//!     let mut res = Response::new();
+//!
+//!     res.set_code(200);
+//!     res.set_content_type("text/plain");
+//!     res.append("Hello, world!");
+//!
+//!     res
+//! }
+//!
+//! fn double_handler(req: &Request) -> Response {
+//!     let mut res = Response::new();
+//!     let to_dbl: i32 = req.get("to_dbl");
+//!
+//!     /* simpler response generation syntax */
+//!     utils::make_response(format!("{}", to_dbl * 2), "text/plain", 200)
+//! }
+//!
+//! fn main() {
+//!     let cnt = Canteen::new(("127.0.0.1", 8080));
+//!
+//!     // set the default route handler to show a 404 message
+//!     cnt.set_default(utils::err_404);
+//!
+//!     // respond to requests to / with "Hello, world!"
+//!     cnt.add_route("/", &[Method::Get], hello_handler);
+//!
+//!     // pull a variable from the path and do something with it
+//!     cnt.add_route("/double/<int:to_dbl>", &[Method::Get], double_handler);
+//!
+//!     // serve raw files from the /static/ directory
+//!     cnt.add_route("/static/<path:path>", &[Method::Get], utils::static_file);
+//!
+//!     cnt.run();
+//! }
+//! ```
 
 extern crate mio;
 extern crate regex;
@@ -26,8 +106,6 @@ use mio::tcp::{TcpListener, TcpStream};
 use mio::util::Slab;
 use mio::*;
 
-pub use utils::*;
-pub use route::*;
 pub use request::*;
 pub use response::*;
 
@@ -75,23 +153,22 @@ impl Client {
         Ok(false)
     }
 
-    /* write the client's output buffer to the socket.
-     *
-     * the following return values mean:
-     *  - Ok(true):  we can close the connection
-     *  - Ok(false): keep listening for writeable event and continue next time
-     *  - Err(e):    something dun fucked up
-     */
+    // write the client's output buffer to the socket.
+    //
+    // the following return values mean:
+    //  - Ok(true):  we can close the connection
+    //  - Ok(false): keep listening for writeable event and continue next time
+    //  - Err(e):    something dun fucked up
     fn send(&mut self) -> Result<bool> {
         while self.o_buf.len() > 0 {
             match self.sock.write(&self.o_buf.as_slice()) {
                 Ok(sz)  => {
                     if sz == self.o_buf.len() {
-                        /* we did it! */
+                        // we did it!
                         self.events.remove(EventSet::writable());
                         break;
                     } else {
-                        /* keep going */
+                        // keep going
                         self.o_buf = self.o_buf.split_off(sz);
                     }
                 },
@@ -121,12 +198,11 @@ impl Client {
     }
 }
 
-/* our primary object. similar interface to Flask, the
- * Python microframework. much faster, however! :)
- */
+/// The primary struct provided by the library. The aim is to have a similar
+/// interface to Flask, the Python microframework.
 pub struct Canteen {
-    routes:  HashMap<RouteDef, Route>,
-    rcache:  HashMap<RouteDef, RouteDef>,
+    routes:  HashMap<route::RouteDef, route::Route>,
+    rcache:  HashMap<route::RouteDef, route::RouteDef>,
     server:  TcpListener,
     token:   Token,
     conns:   Slab<Client>,
@@ -169,6 +245,13 @@ impl Handler for Canteen {
 }
 
 impl Canteen {
+    /// Creates a new Canteen instance.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let cnt = Canteen::new(("127.0.0.1", 8080));
+    /// ```
     pub fn new<A: ToSocketAddrs>(addr: A) -> Canteen {
         Canteen {
             routes:  HashMap::new(),
@@ -176,21 +259,35 @@ impl Canteen {
             server:  TcpListener::bind(&addr.to_socket_addrs().unwrap().next().unwrap()).unwrap(),
             token:   Token(1),
             conns:   Slab::new_starting_at(Token(2), 2048),
-            default: Route::err_404,
+            default: utils::err_404,
         }
     }
 
+    /// Adds a new route definition to be handled by Canteen.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// fn handler(_: &Request) -> Response {
+    ///     utils::make_response("<b>Hello, world!</b>", "text/html", 200)
+    /// }
+    ///
+    /// fn main() {
+    ///     let cnt = Canteen::new(("127.0.0.1", 8080));
+    ///     cnt.add_route("/hello", &[Request::Get], handler);
+    /// }
+    /// ```
     pub fn add_route(&mut self, path: &str, mlist: &[Method],
                      handler: fn(&Request) -> Response) -> &mut Canteen {
         let mut methods: HashSet<Method> = HashSet::new();
 
-        /* make them unique */
+        // make them unique
         for m in mlist {
             methods.insert(*m);
         }
 
         for m in methods {
-            let rd = RouteDef {
+            let rd = route::RouteDef {
                 pathdef:    String::from(path),
                 method:     m,
             };
@@ -199,12 +296,20 @@ impl Canteen {
                 panic!("a route handler for {} has already been defined!", path);
             }
 
-            self.routes.insert(rd, Route::new(&path, m, handler));
+            self.routes.insert(rd, route::Route::new(&path, m, handler));
         }
 
         self
     }
 
+    /// Defines a default route for undefined paths.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let cnt = Canteen::new(("127.0.0.1", 8080));
+    /// cnt.set_default(canteen::utils::err_404);
+    /// ```
     pub fn set_default(&mut self, handler: fn(&Request) -> Response) {
         self.default = handler;
     }
@@ -246,7 +351,7 @@ impl Canteen {
     }
 
     fn handle_request(&mut self, req: &mut Request) -> Vec<u8> {
-        let resolved = RouteDef { pathdef: req.path.clone(), method: req.method };
+        let resolved = route::RouteDef { pathdef: req.path.clone(), method: req.method };
         let mut handler: fn(&Request) -> Response = self.default;
 
         if self.rcache.contains_key(&resolved) {
@@ -293,7 +398,7 @@ impl Canteen {
     }
 
     fn reset_connection(&mut self, token: Token) {
-        /* kill the connection */
+        // kill the connection
         self.conns.remove(token);
     }
 
@@ -315,6 +420,13 @@ impl Canteen {
         };
     }
 
+    /// Creates the listener and starts a Canteen server's event loop.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let cnt = Canteen::new(("127.0.0.1", 8080));
+    /// ```
     pub fn run(&mut self) {
         let mut evl = EventLoop::new().unwrap();
         self.register(&mut evl).ok();
