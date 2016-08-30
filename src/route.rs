9fbb03e56ad5b90f1fb1a31fa4f02db731052c5f
Jeff Nettleton <jeffdn@gmail.com>
2016-08-29 21:27:46 -0400

    add documentation to canteen::route, move some general functions to canteen::utils

--- src/route.rs
+++ src/route.rs
@@ -8,31 +8,30 @@
 
 extern crate regex;
 
-use std::env;
-use std::fs::File;
-use std::error::Error;
-use std::path::PathBuf;
-use std::io::prelude::*;
 use std::collections::HashMap;
 use regex::Regex;
 
 use request::*;
 use response::*;
 
+// The various types of parameters that can be contained in a URI.
 #[derive(PartialEq, Eq, Hash, Debug)]
-pub enum ParamType {
+enum ParamType {
     Integer,
     String,
     Float,
     Path,
 }
 
+/// This struct represents a route definition. It is only necessary for
+/// use internally.
 #[derive(PartialEq, Eq, Hash, Debug, Clone)]
 pub struct RouteDef {
     pub pathdef: String,
     pub method:  Method,
 }
 
+/// This struct defines a route or endpoint.
 pub struct Route {
     matcher:     Regex,
     method:      Method,
@@ -41,6 +40,7 @@ pub struct Route {
 }
 
 impl Route {
+    /// Create a new Route. This function is called by the Canteen struct.
     pub fn new(path: &str, method: Method, handler: fn(&Request) -> Response) -> Route {
         let re = Regex::new(r"^<(?:(int|str|float|path):)?([\w_][a-zA-Z0-9_]*)>$").unwrap();
         let parts: Vec<&str> = path.split('/').filter(|&s| s != "").collect();
@@ -99,10 +99,12 @@ impl Route {
         }
     }
 
+    /// Check if this Route matches a given URI.
     pub fn is_match(&self, req: &Request) -> bool {
         self.matcher.is_match(&req.path) && self.method == req.method
     }
 
+    /// Parse and extract the variables from a URI based on this Route's definition.
     pub fn parse(&self, path: &str) -> HashMap<String, String> {
         let mut params: HashMap<String, String> = HashMap::new();
 
@@ -117,87 +119,5 @@ impl Route {
         }
 
         params
-    }
-
-    pub fn replace_escape(path: &str) -> String {
-        let mut fixed = String::from(path);
-        let replaces: [(&str, &str); 22] = [
-                ("%20", " "),
-                ("%3C", "<"),
-                ("%3E", ">"),
-                ("%23", "#"),
-                ("%25", "%"),
-                ("%7B", "["),
-                ("%7D", ")"),
-                ("%7C", "|"),
-                ("%5C", "\\"),
-                ("%5E", "^"),
-                ("%7E", "~"),
-                ("%5B", "["),
-                ("%5D", ")"),
-                ("%60", "`"),
-                ("%3B", ";"),
-                ("%2F", "/"),
-                ("%3F", "?"),
-                ("%3A", ":"),
-                ("%40", "@"),
-                ("%3D", "="),
-                ("%26", "&"),
-                ("%24", "$"),
-        ];
-
-        for &(from, to) in replaces.iter() {
-            fixed = fixed.replace(from, to);
-        }
-
-        fixed
-    }
-
-    pub fn err_403(req: &Request) -> Response {
-        Response::err_403(&req.path)
-    }
-
-    pub fn err_404(req: &Request) -> Response {
-        Response::err_404(&req.path)
-    }
-
-    pub fn static_file(req: &Request) -> Response {
-        let mut res = Response::new();
-
-        let cwd = env::current_dir().unwrap();
-        let clean = Route::replace_escape(&req.path);
-        let mut fpath = PathBuf::from(&cwd);
-        let mut fbuf: Vec<u8> = Vec::new();
-
-        for chunk in clean.split('/') {
-            if chunk == "" || chunk == "." || chunk == ".." {
-                /* bzzzzt */
-                continue;
-            }
-
-            fpath.push(&chunk);
-        }
-
-        let file = File::open(&fpath);
-
-        match file {
-            Ok(mut f)   => {
-                match f.read_to_end(&mut fbuf) {
-                    Ok(_)   => {
-                        res.set_code(200);
-                        res.set_content_type("text/plain");
-                        res.append(fbuf);
-                    },
-                    Err(e)  => {
-                        return Response::err_500(e.description());
-                    },
-                }
-            },
-            Err(_)      => {
-                return Response::err_404(&req.path);
-            }
-        }
-
-        res
     }
 }
