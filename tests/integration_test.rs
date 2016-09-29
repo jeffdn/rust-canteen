0def1568e0bd2566e3dadac9709aa4c9c3cd25c3
Jeff Nettleton <jeffdn@gmail.com>
2016-09-28 22:04:07 -0400

    add three integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -10,51 +10,74 @@ use std::thread;
 use std::io::prelude::*;
 
 fn hello_handler(_: &Request) -> Response {
-    let mut res = Response::new();
-
-    res.set_code(200);
-    res.set_content_type("text/plain");
-    res.append("Hello, world!");
-
-    res
+    utils::make_response("Hello, world!", "text/plain", 200)
 }
 
 fn double_handler(req: &Request) -> Response {
-    let mut res = Response::new();
     let to_dbl: i32 = req.get("to_dbl");
+    utils::make_response(format!("{}", to_dbl * 2), "text/plain", 200)
+}
 
-    res.set_code(200);
-    res.set_content_type("text/plain");
-    res.append(format!("{}", to_dbl * 2));
+fn complex_handler(req: &Request) -> Response {
+    let site_id: i32 = req.get("site_id");
+    let user_id: i32 = req.get("user_id");
+    let prod_id: String = req.get("prod_id");
+    let result = format!("{{ site_id: {}, user_id: {}, prod_id: \"{}\" }}", site_id, user_id, prod_id);
 
-    res
+    utils::make_response(result, "text/plain", 200)
 }
 
 #[test]
-#[should_panic]
-fn main() {
+fn test_no_route() {
     let th = thread::spawn(|| {
-        let mut cnt = Canteen::new(("127.0.0.1", 8888));
-
-        cnt.add_route("/", &[Method::Get], hello_handler)
-           .add_route("/double/<int:to_dbl>", &[Method::Get], double_handler)
-           .set_default(utils::err_404);
-
-        cnt._test();
+        let mut cnt = Canteen::new(("127.0.0.1", 8881));
+        cnt.add_route("/", &[Method::Get], hello_handler);
+        cnt.run();
     });
 
     let client = Client::new();
 
-    let mut res_double = client.get("http://localhost:8888/double/8").send().unwrap();
-    let mut val_double = String::new();
-    let _ = res_double.read_to_string(&mut val_double).unwrap();
-    assert_eq!("16", val_double);
+    let mut res = client.get("http://localhost:8881/").send().unwrap();
+    let mut val = String::new();
+    let _ = res.read_to_string(&mut val).unwrap();
+    assert_eq!("Hello, world!", val);
 
-    let mut res_hello = client.get("http://localhost:8888/").send().unwrap();
-    let mut val_hello = String::new();
-    let _ = res_hello.read_to_string(&mut val_hello).unwrap();
-    assert_eq!("Hello, world!", val_hello);
+    let _ = th.join();
+}
 
-    let _ = client.get("http://localhost:8888/__kill").send().unwrap();
+#[test]
+fn test_int_route() {
+    let th = thread::spawn(|| {
+        let mut cnt = Canteen::new(("127.0.0.1", 8882));
+        cnt.add_route("/double/<int:to_dbl>", &[Method::Get], double_handler);
+        cnt.run();
+    });
+
+    let client = Client::new();
+
+    let mut res = client.get("http://localhost:8882/double/8").send().unwrap();
+    let mut val = String::new();
+    let _ = res.read_to_string(&mut val).unwrap();
+    assert_eq!("16", val);
+
+    let _ = th.join();
+}
+
+#[test]
+fn test_complex_route() {
+    let th = thread::spawn(|| {
+        let mut cnt = Canteen::new(("127.0.0.1", 8883));
+        cnt.add_route("/api/v1/site/<int:site_id>/user/<int:user_id>/product/<str:prod_id>",
+                      &[Method::Get], complex_handler);
+        cnt.run();
+    });
+
+    let client = Client::new();
+
+    let mut res = client.get("http://localhost:8883/api/v1/site/7/user/382/product/bf73a9cc0").send().unwrap();
+    let mut val = String::new();
+    let _ = res.read_to_string(&mut val).unwrap();
+    assert_eq!("{ site_id: 7, user_id: 382, prod_id: \"bf73a9cc0\" }", val);
+
     let _ = th.join();
 }
